9f548666bc60ddac61b5d3f12772789d7a29e730
fendijatmiko <jatmikofendi@gmail.com>
2016-12-31 06:13:11 +0700

    edit a few line on about page except this time in angular module

--- app_client/about/about.controller.js
+++ app_client/about/about.controller.js
@@ -9,9 +9,7 @@
             title: 'About LocYoben',
         };
         vm.main = {
-            content: 'LocYoben was created to help people find places to sit down '+
-                     'and get a bit of work done.\n\nLorem ipsum dolor sit amet,'+
-                     ' consectetur adipiscing elit.'
+            content: 'LocYoben was created in order to complete candidature for s1 academic degree in akakom.\n\nAplikasi ini dirancang untuk memenuhi kebutuhan pengunjung kafe yang memiliki fasilitas hotspot untuk menentukan fasilitas, hidangan serta pelayanan yang ditawarkan dari masing – masing kafe, melalui aplikasi ini pengunjung juga dapat memberikan review atau feedback mengenai kafe yang mereka kunjungi menggunakan teklonogi – teknologi terbaru dalam dunia web developmen dan cloud computing seperti non-blocking io dan Single Page Application (SPA).         “Aplikasi Berbasis Web untuk Pencarian Hotspot Kafe Menggunakan MEAN Stack (MongoDb, ExpressJs, AngularJs dan NodeJs)”.'
         };
     }
 })();
